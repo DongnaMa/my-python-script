25768b33f6057dc3c51985f11482c6ee0a9fd68a
Molly <15380135366@163.com>
2019-08-02 15:52:00 +0800

    Update Cross_output.py

--- Cross_output.py
+++ Cross_output.py
@@ -9,7 +9,7 @@ for line1,line2 in zip_longest(file1,file2):
     line1 = line1.strip()
     line2 = line2.strip()
     out_result.write(line1+"\n"+line2+"\n")
+    out_result.close()
 
-out_result.close()
 file1.close()
 file2.close()

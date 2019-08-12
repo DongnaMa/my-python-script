a2b8b6090cf58466bee0afaadb9573e5ccc3ba18
Molly <15380135366@163.com>
2019-08-12 11:16:31 +0800

    Update property.py

--- property.py
+++ property.py
@@ -1,4 +1,5 @@
 #如果c是C的实例化,c.x 将触发getSize,c.x = value将触发setSize,size将赋值为1,即c.size=1,del c.x 触发 delSize,使size的值删除
+#通过使用property使x与size相连
 class C:
     def __init__(self, size = 10):
         self.size = size
